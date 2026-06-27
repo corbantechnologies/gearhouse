3b3b72b81689973c194a07afcc14d7853c9d5f03
Dalienst Owino <oduordalienst@gmail.com>
2026-06-28 00:03:25 +0300

    add logs

--- app/(private)/vendor/pos-sales/[reference]/page.tsx
+++ app/(private)/vendor/pos-sales/[reference]/page.tsx
@@ -33,6 +33,7 @@ export default function POSSaleDetailsPage() {
         toast.success("Receipt downloaded!");
       },
       onError: (err: any) => {
+        console.log(err);
         toast.error(err?.response?.data?.error || "Failed to download receipt.");
       }
     });
